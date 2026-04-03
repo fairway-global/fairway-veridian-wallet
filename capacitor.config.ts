65ec090e9fe474dcae24ad851fa8c64f6a59bfb6
Ermias Bunaro <ermiarch@gmail.com>
2026-04-03 13:08:01 +0300

    update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import { CapacitorConfig } from "@capacitor/cli";
 import { KeyboardResize } from "@capacitor/keyboard";
 
 const config: CapacitorConfig = {
-  appId: "org.cardanofoundation.idw",
-  appName: "Fairwallet",
+  appId: "global.fairway.faydaidw",
+  appName: "FairwayIdentityWallet",
   webDir: "build",
   android: {
     webContentsDebuggingEnabled: process.env.ENVIRONMENT !== "prod",
